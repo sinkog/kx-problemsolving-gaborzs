a1dc5d330ff2e08f91b3fc9954c1814a5119d9b3
Sinkó Gábor <gabor.zoltan.sinko@gmail.com>
2024-10-26 16:09:32 +0200

    pylint bugs

--- storage-service/src/app.py
+++ storage-service/src/app.py
@@ -9,26 +9,25 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Dummy data
-data = {
-    "message": "Hello, this is dummy data from the Storage Service"
-}
-alive = {
-    "state": "OK"
-}
+data = {"message": "Hello, this is dummy data from the Storage Service"}
+alive = {"state": "OK"}
 
-@app.route('/data', methods=['GET'])
+
+@app.route("/data", methods=["GET"])
 def get_data():
     """
     Get data from the API.
     """
     return jsonify(data)
 
-@app.route('/state', methods=['GET'])
+
+@app.route("/state", methods=["GET"])
 def get_state():
     """
     Get the current state of the service.
     """
     return jsonify(alive)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Specify the appropriate port
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Specify the appropriate port
