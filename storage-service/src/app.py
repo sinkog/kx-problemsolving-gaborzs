1acae491823c6d987e9b4ceb81bfea836e6ceefa
Sinkó Gábor <gabor.zoltan.sinko@gmail.com>
2024-10-26 09:39:40 +0200

    before Brack

--- storage-service/src/app.py
+++ storage-service/src/app.py
@@ -5,20 +5,19 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Dummy adatok
-data = {
-    "message": "Hello, this is dummy data from the Storage Service"
-}
-alive = {
-    "state": "OK"
-}
+data = {"message": "Hello, this is dummy data from the Storage Service"}
+alive = {"state": "OK"}
 
-@app.route('/data', methods=['GET'])
+
+@app.route("/data", methods=["GET"])
 def get_data():
     return jsonify(data)
 
-@app.route('/state', methods=['GET'])
+
+@app.route("/state", methods=["GET"])
 def get_state():
     return jsonify(alive)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # A megfelelő port megadása
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # A megfelelő port megadása
