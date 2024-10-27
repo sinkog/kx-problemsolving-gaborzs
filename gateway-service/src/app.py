dbe4719ba090e3d5306cd6de334005a0a829b37a
Sinkó Gábor <gabor.zoltan.sinko@gmail.com>
2024-10-27 10:25:57 +0100

    check_services

--- gateway-service/src/app.py
+++ gateway-service/src/app.py
@@ -25,6 +25,34 @@ def initialize_services():
   print(f"init {service_statuses}")
   return storage_services, service_statuses
 
+async def check_service(url, service_name):
+    print(f"cs: {url}/status")
+    print(f"cs:{service_name}")
+    async with aiohttp.ClientSession() as session:
+        try:
+            # Check the status endpoint
+            print(f"cs_{url}/status XXX")
+            async with session.get(f"{url}/status") as response:
+                # Check if response is OK and contains "OK"
+                print(f"cs {response.status}")
+                if response.status == 200:
+                    json_response = await response.json()
+                    if json_response.get("status") == "OK":  # Assuming the response has a "status" field
+                        print("cs: OK")
+                        service_statuses[service_name] = ServiceStatus.AVAILABLE
+                    else:
+                        print("cs: <>OK")
+                        service_statuses[service_name] = ServiceStatus.UNAVAILABLE
+                else:
+                    print("cs: <>200")
+                    print(f"cd: {response.status}")
+                    service_statuses[service_name] = ServiceStatus.UNAVAILABLE
+        except Exception:
+            print("cs: except")
+            service_statuses[service_name] = ServiceStatus.UNAVAILABLE
+            print(service_statuses)
+
+
 async def monitor_services():
     print(f"ms: {storage_services}")
     tasks = [check_service(url, f"storage_service_{idx + 1}") for idx, url in enumerate(storage_services)]
