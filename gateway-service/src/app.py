de58095a1e84d9aa522ca59c3b0c4dcb9faeb36b
Sinkó Gábor <gabor.zoltan.sinko@gmail.com>
2024-10-27 13:22:26 +0100

    print to debug info

--- gateway-service/src/app.py
+++ gateway-service/src/app.py
@@ -1,6 +1,8 @@
 import os
 import asyncio
 import aiohttp
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
 from flask import Flask, jsonify
 from enum import Enum
 
@@ -19,39 +21,37 @@ class ServiceStatus(Enum):
 def initialize_services(function_run=True):
   global storage_services
   storage_services = os.getenv("STORAGE_SERVICES", "").split(",") if os.getenv("STORAGE_SERVICES") else []
-  print(f"init {storage_services}")
+  logging.debug(f"{storage_services}")
   # Status dictionary to keep track of services
   global service_statuses
   service_statuses = {f"storage_service_{idx + 1}": ServiceStatus.UNAVAILABLE for idx in range(len(storage_services))}
-  print(f"init {service_statuses}")
+  logging.debug(f"{service_statuses}")
   global func_run
   func_run = function_run
   return storage_services, service_statuses
 
 async def check_service(url, service_name):
-    print(f"cs: {url}/status")
-    print(f"cs:{service_name}")
+    logging.debug(f"{url}/status")
+    logging.debug(f"{service_name}")
     async with aiohttp.ClientSession() as session:
         try:
             # Check the status endpoint
-            print(f"cs_{url}/status XXX")
             async with session.get(f"{url}/status") as response:
                 # Check if response is OK and contains "OK"
-                print(f"cs {response.status}")
+                logging.debug(f"{response.status}")
                 if response.status == 200:
                     json_response = await response.json()
                     if json_response.get("status") == "OK":  # Assuming the response has a "status" field
-                        print("cs: OK")
+                        logging.debug("OK")
                         service_statuses[service_name] = ServiceStatus.AVAILABLE
                     else:
-                        print("cs: <>OK")
+                        logging.debug("<>OK")
                         service_statuses[service_name] = ServiceStatus.UNAVAILABLE
                 else:
-                    print("cs: <>200")
-                    print(f"cd: {response.status}")
+                    logging.debug(f"{response.status}<>200")
                     service_statuses[service_name] = ServiceStatus.UNAVAILABLE
         except Exception:
-            print("cs: except")
+            logging.debug("except")
             service_statuses[service_name] = ServiceStatus.UNAVAILABLE
             print(service_statuses)
 
@@ -61,19 +61,18 @@ async def monitor_service(url, service_name):
     run = True
     while run:
         await check_service(url, service_name)
-        print(func_run)
         if  service_statuses[service_name] == ServiceStatus.AVAILABLE:
-            print(f"{service_name} is available. Checking again in 1 second.")
+            logging.debug(f"{service_name} is available. Checking again in 1 second.")
             await asyncio.sleep(1)
         else:
-            print(f"{service_name} is not available. Checking again in 5 seconds.")
+            logging.debug(f"{service_name} is not available. Checking again in 5 seconds.")
             await asyncio.sleep(5)
         run = func_run
 
 async def monitor_services():
-    print(f"ms: {storage_services}")
+    logging.debug(f"{storage_services}")
     tasks = [monitor_service(url, f"storage_service_{idx + 1}") for idx, url in enumerate(storage_services)]
-    print("Tasks being created:", tasks)
+    logging.debug("Tasks being created:", tasks)
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
